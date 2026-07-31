5634a5837a4cecb3425f081c3aee52806b3ff709
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2026-07-31 23:21:03 +1200

    finalised what component to select in the alibi
    selected what goes into what list and created states for them to be used in actual lists - with their value being the specific key it needs it needs to look for to put in the alibi, or in some cases, to derive something to put in the alibi

--- states.py
+++ states.py
@@ -37,6 +37,9 @@ class NoteStates(Enum):
 
 
 
+
+
+
 # this class stored all the suspect states
 class SuspectStates(Enum):
     DUMMY = 'dummy suspect' # dummy suspects exist only because certain of their features seem to match the ones found as clues
@@ -44,3 +47,29 @@ class SuspectStates(Enum):
     OPPURTUNITY = 'oppurtunity only' # these suspects only have oppurtunity
     DUMMY_CULPRIT = 'motive + oppurtunity + inaction' # these suspects had the same conditions as the culprit with the victim, but did not take the leap of action 
     CULPRIT = 'motive + oppurtunity + action' # this is the culprit
+
+
+
+
+
+
+
+
+
+# these are the states for the alibi components that the culprit can lie about, and mislead the cops
+class CrucialAlibiComponentStates(Enum):
+    # these are the crucial alibi components that the culprit can lie about, and mislead the cops
+    LOCATION_DURING_CRIME = 'location during crime'
+    RELATIONSHIP_WITH_VICTIM = 'suspect status'
+    MURDER_MOTIVE = 'suspect motives'
+    BLOOD_TYPE = 'blood type' # this looks at the suspect blood type
+    WITNESS_STATEMENT = 'is witness suspect' # i used the bool that tracks if a suspect is a witness, because then i can manually add the witness component to the final components for the alibi, if a witness is also a suspect - this can be useful to add a chance system for if the culprit is selected as a witness as well - i can code a system where there is a chance that the culprit's alibi/statement does not match their own witness statement, which can be seen as a careless mistake, or the effect of the other option will be that the culprit's alibi as a suspect and witness will remain the same, making the statement airtight and harder to solve
+
+class NonCrucialAlibiComponentStates(Enum):
+    # these are the non crucial alibi components that the culprit can lie about, but will not mislead the cops
+    WEATHER = 'weather'
+    LOCATION_BEFORE_CRIME = 'location before crime'
+    LOCATION_AFTER_CRIME = 'location after crime'
+    CLOTHING_TOP = 'top clothing'
+    CLOTHING_BOTTOM = 'bottom clothing'
+    # will try to not explicitly use color of top and bottom as they are part of the clothing, and not a separate alibi component - but if found that they both have to be separate states as well, will add them here later.
