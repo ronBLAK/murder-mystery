76a396826d5a296543dd2deb7172cc4b195dfdfd
ronBLAK <159859853+ronBLAK@users.noreply.github.com>
2026-08-02 14:49:33 +1200

    added the selected states for alibi components into their respective lists

--- alibis.py
+++ alibis.py
@@ -1,6 +1,23 @@
+from states import AlibiComponentStates
+
 class AlibiComponentFramework:
-    crucial_case_components = [] # list to store the crucial components of the case, which when lied about, will lead to the the cops being mislead - this list is manually populated
-    non_crucial_case_components = [] # list to store the non crucial components of the case, which when lied about, will not lead to the cops being mislead. this list is manually populated
+    # list to store the crucial components of the case, which when lied about, will lead to the the cops being mislead - this list is manually populated
+    crucial_case_components = [
+        AlibiComponentStates.LOCATION_DURING_CRIME,
+        AlibiComponentStates.RELATIONSHIP_WITH_VICTIM,
+        AlibiComponentStates.MURDER_MOTIVE,
+        AlibiComponentStates.BLOOD_TYPE,
+        AlibiComponentStates.WITNESS_STATEMENT
+    ]
+
+    # list to store the non crucial components of the case, which when lied about, will not lead to the cops being mislead. this list is manually populated
+    non_crucial_case_components = [
+        AlibiComponentStates.WEATHER,
+        AlibiComponentStates.LOCATION_BEFORE_CRIME,
+        AlibiComponentStates.LOCATION_AFTER_CRIME,
+        AlibiComponentStates.CLOTHING_TOP,
+        AlibiComponentStates.CLOTHING_BOTTOM
+    ]
 
 class Alibis:
     def generate_alibi_data(suspect_number, alibi_components):
